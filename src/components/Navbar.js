93a34895ddf994bb0451fc1081473c9a2a7b9181
SLASH-7 <basnetujjwal98@gmail.com>
2023-08-17 17:01:42 +0545

    nav complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,98 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { loginWithRedirect, logout, user, isAuthenticated, isLoading } =
+    useAuth0();
+  const [open, setOpen] = useState(false);
+  const menuRef = useRef();
+  const hamRef = useRef();
+  useEffect(() => {
+    let handle = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handle);
+    return () => document.removeEventListener("mousedown", handle);
+  });
   return (
-    <nav className="w-full h-16 bg-slate-200 ">
-      <section className="max-w-7xl mx-auto w-full h-16 flex justify-between items-center">
-        <h1>SLASH</h1>
-        <ul className="flex gap-7">
+    <nav className="fixed h-20 w-full text-white z-50">
+      <section className="max-w-7xl mx-auto flex flex-wrap h-20 justify-between items-center px-4">
+        <div data-aos="fade-right">
+          <h1 className="text-2xl font-bold">SLASH</h1>
+        </div>
+        <ul className="lg:flex hidden text-sm gap-x-10 ">
           <li>
-            <Link to="/home">Home</Link>
+            <Link to="/">HOME</Link>
           </li>
           <li>
-            <Link to="/home">Services</Link>
+            <Link to="/service">SERVICE</Link>
           </li>
           <li>
-            <Link to="/home">About Us</Link>
+            <Link to="/contact">CONTACT</Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <span className="pr-1">
+                <ion-icon name="cart"></ion-icon>
+              </span>
+              CART
+            </Link>
+          </li>
+          {!isAuthenticated ? (
+            <>
+              <button
+                onClick={() => loginWithRedirect()}
+                className="bg-white rounded-md px-2 text-gray-900"
+              >
+                <ion-icon name="person"></ion-icon>
+                LOG IN
+              </button>
+            </>
+          ) : (
+            <button
+              className="bg-white rounded-md px-2 text-gray-900"
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              LOG OUT
+            </button>
+          )}
+        </ul>
+        <ul
+          ref={menuRef}
+          className={`lg:hidden absolute text-gray-900  bg-white bg-opacity-30 backdrop-blur shadow-black shadow-2xl
+          rounded-bl-full w-2/4 h-screen top-0  ham  text-xl text-center pt-20 transition-all ease-in duration-500
+          ${open ? "right-0" : "right-[-32rem]"}
+          `}
+        >
+          <li className="py-5 lg:py-0">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="py-5 lg:py-0">
+            <Link to="/service">SERVICE</Link>
+          </li>
+          <li className="py-5 lg:py-0 ">
+            <span>
+              <ion-icon name="cart"></ion-icon>
+            </span>
+            <Link to="/home">CART</Link>
+          </li>
+          <li className="py-5 lg:py-0">
+            <Link to="/contact">CONTACT</Link>
           </li>
         </ul>
+        <div
+          ref={hamRef}
+          className="lg:hidden text-3xl text-white"
+          onClick={() => setOpen(!open)}
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
+        </div>
       </section>
     </nav>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="h-screen w-full bg-[url('./images/main.jpg')] bg-cover bg-blue-300"></div>
+    <div className="lg:h-screen h-[50rem] w-full bg-[url('./images/main.jpg')] bg-cover">
+      <div className="h-screen w-full bg-black bg-opacity-40">
+        <div className="text-white max-w-7xl mx-auto pt-80 px-4">
+          <p className="text-6xl font-semibold w-[70%] pb-4">
+            Stay productive and get more work done!
+          </p>
+          <button className="h-10 w-32 bg-gray-600 rounded-md">
+            Shop More
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
