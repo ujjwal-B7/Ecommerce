bc4648ba9b2354c3e613570ad88c94463284ecde
SLASH-7 <basnetujjwal98@gmail.com>
2023-12-24 12:27:34 +0545

    form optimized

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const LoginForm = ({ showForm, setShowForm }) => {
+  function showPassword() {
+    let password = document.getElementById("password");
+    if (password.type === "password") {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  }
   return (
     <>
       <div
@@ -8,9 +16,10 @@ const LoginForm = ({ showForm, setShowForm }) => {
       `}
       >
         <form
-          className={`max-w-sm relative mx-auto bg-white/90 w-11/12 p-5 rounded-xl
+          className={`max-w-sm relative mx-auto bg-white/95 w-11/12 p-5 rounded-xl shadow-xl
          ${showForm ? "hidden" : "block"}
         `}
+          // data-aos="fade-up"
         >
           <div
             className="text-gray-900 h-5 w-5 pl-[1px] hover:bg-gray-400 absolute right-2 top-2 rounded-full"
@@ -25,7 +34,7 @@ const LoginForm = ({ showForm, setShowForm }) => {
           </p>
           <div className="my-5">
             <label
-              for="email"
+              htmlFor="email"
               className="block mb-2 text-md font-semibold text-gray-900"
             >
               Your email
@@ -34,15 +43,15 @@ const LoginForm = ({ showForm, setShowForm }) => {
             <input
               type="email"
               id="email"
-              className=" border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
-              style={{ backgroundColor: "red" }}
+              className=" border border-gray-900 bg-transparent  text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
+              // style={{ backgroundColor: "gray" }}
               placeholder="abc@gmail.com"
               required
             />
           </div>
           <div className="mb-5">
             <label
-              for="password"
+              htmlFor="password"
               className="block mb-2 text-md font-semibold text-gray-900"
             >
               Your password
@@ -50,7 +59,7 @@ const LoginForm = ({ showForm, setShowForm }) => {
             <input
               type="password"
               id="password"
-              className="border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
+              className="border border-gray-900 bg-transparent text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
               placeholder="********"
               required
             />
@@ -61,12 +70,13 @@ const LoginForm = ({ showForm, setShowForm }) => {
                 id="remember"
                 type="checkbox"
                 value=""
-                className="w-4 h-4 border border-gray-900 rounded focus:ring-3 focus:ring-gray-300 "
+                className="text-gray-900 w-3 h-3 mt-[5px] border bg-transparent border-gray-900 rounded focus:ring-3 focus:ring-gray-300 "
                 required
+                onClick={showPassword}
               />
             </div>
-            <label for="remember" className="ms-2 text-md text-gray-900">
-              Remember me
+            <label htmlFor="remember" className="ms-2 text-md  text-gray-900">
+              show password
             </label>
           </div>
           <button
