3a62e09b0c1c84ffebca24a400ea48c00fc77f73
SLASH-7 <basnetujjwal98@gmail.com>
2023-12-24 20:25:36 +0545

    form completed

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,33 +1,27 @@
 import React from "react";
 import { useState } from "react";
-import RegisterForm from "./RegisterForm";
-const LoginForm = ({ showForm, setShowForm }) => {
+const RegisterForm = ({
+  showRegisterForm,
+  setShowRegisterForm,
+  shiftToRegister,
+  setShiftToRegister,
+}) => {
   const [showPassword, setShowPassword] = useState(true);
-  const [showRegisterForm, setShowRegisterForm] = useState(true);
-  const [shiftToRegister, setShiftToRegister] = useState(false);
+
   return (
     <>
-      <div
-        className={`fixed top-0 left-0 w-full h-screen bg-black bg-opacity-50 flex justify-center items-center z-50
-      `}
-      >
-        <form
-          className={`max-w-sm relative mx-auto bg-white/95 w-11/12 p-5 rounded-xl shadow-xl
-         ${showForm ? "hidden" : "block"}
-         ${shiftToRegister ? "hidden" : "block"}
+      <form
+        className={`max-w-sm relative mx-auto bg-white/95 w-11/12 p-5 rounded-xl shadow-xl
+          ${showRegisterForm ? "hidden" : "block"}
+         
         `}
-          // data-aos="fade-up"
-        >
-          <div
-            className="text-gray-900 h-5 w-5 pl-[1px] hover:bg-gray-400 absolute right-2 top-2 rounded-full"
-            onClick={() => {
-              setShowForm(!showForm);
-            }}
-          >
-            <ion-icon name="close" size="small"></ion-icon>
-          </div>
+      >
+        {/* <div className="text-gray-900 h-5 w-5 pl-[1px] hover:bg-gray-400 absolute right-2 top-2 rounded-full">
+          <ion-icon name="close" size="small"></ion-icon>
+        </div> */}
+        <div data-aos="fade-up">
           <p className="text-center text-gray-900 text-xl font-semibold">
-            Login Form
+            Register Your Account
           </p>
           <div className="my-5">
             <label
@@ -61,6 +55,21 @@ const LoginForm = ({ showForm, setShowForm }) => {
               required
             />
           </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-md font-semibold text-gray-900"
+            >
+              Confirm Your password
+            </label>
+            <input
+              type={showPassword ? "password" : "text"}
+              id="password"
+              className="border border-gray-900 bg-transparent text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 "
+              placeholder="********"
+              required
+            />
+          </div>
           <div className="flex items-start mb-5">
             <div className="flex items-center h-5">
               <input
@@ -82,31 +91,19 @@ const LoginForm = ({ showForm, setShowForm }) => {
           >
             Submit
           </button>
-          <p className="text-blue-900  text-sm pt-2 hover:underline cursor-pointer">
-            Forgot Password?
-          </p>
           <p
-            className="text-center font-semibold underline cursor-pointer text-gray-900 text-sm pt-5"
+            className="text-gray-900 text-center pt-5 font-semibold text-sm cursor-pointer underline"
             onClick={() => {
               setShowRegisterForm(!showRegisterForm);
               setShiftToRegister(!shiftToRegister);
             }}
           >
-            Register Account
+            Login instead
           </p>
-        </form>
-
-        <RegisterForm
-          showPassword={setShowForm}
-          setShowPassword={setShowPassword}
-          showRegisterForm={showRegisterForm}
-          setShowRegisterForm={setShowRegisterForm}
-          shiftToRegister={shiftToRegister}
-          setShiftToRegister={setShiftToRegister}
-        />
-      </div>
+        </div>
+      </form>
     </>
   );
 };
 
-export default LoginForm;
+export default RegisterForm;
